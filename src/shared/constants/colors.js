776499f2fd1648e3734a66c170d54f6d698ea1cf
chelshaw <chelshaw.dev@gmail.com>
2019-07-30 22:07:52 -0500

    Add COLORS constants

---
+++ src/shared/constants/colors.js
@@ -0,0 +1,40 @@
+const cyan = '#41B6AC';
+const grass = '#73B641';
+const green = '#1D812B';
+const royal = '#5116DE';
+const magenta = '#DD3787';
+const tangerine = '#FF881F';
+const gray = '#424242';
+const medGray = '#636C74';
+const lightishGray = '#a6a6a6';
+const lightGray = '#E6E6E6';
+const danger = '#b41a1a';
+const warning = '#d4851a';
+const offWhite = '#fafafa';
+const white = '#ffffff';
+
+export default {
+  cyan,
+  grass,
+  green,
+  royal,
+  magenta,
+  tangerine,
+  gray,
+  medGray,
+  lightishGray,
+  lightGray,
+  offWhite,
+  white,
+  tabIconDefault: medGray,
+  tabIconSelected: grass,
+  tabBar: lightGray,
+  errorBackground: danger,
+  errorText: white,
+  warningBackground: warning,
+  warningText: white,
+  noticeBackground: cyan,
+  noticeText: white,
+  danger,
+  warning,
+};
