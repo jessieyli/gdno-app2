9e12f61057933e69ddff73ecb92df008dcdd44e6
chelshaw <chelshaw.dev@gmail.com>
2020-01-16 22:45:07 -0600

    chore: Add Async Storage package from rn community @1.7.1

--- src/shared/data/localStorage.js
+++ src/shared/data/localStorage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import handleError from './handleError';
 
 export const keyTypes = {
