110a7e028e787448423ea1a17a4a5ad833088888
chelshaw <chelshaw.dev@gmail.com>
2020-01-24 07:52:25 -0600

    Revert "chore: Add Async Storage package from rn community @1.7.1"
    This reverts commit 9e12f61057933e69ddff73ecb92df008dcdd44e6.

--- android/app/src/main/java/com/gdno/MainApplication.java
+++ android/app/src/main/java/com/gdno/MainApplication.java
@@ -3,7 +3,6 @@ package com.gdno;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import io.sentry.RNSentryPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -42,7 +41,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AsyncStoragePackage(),
             new RNSentryPackage(),
             new SvgPackage(),
           new ReanimatedPackage(),
